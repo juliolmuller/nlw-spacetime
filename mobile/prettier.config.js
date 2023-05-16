d2427dd35f42f53d13ef3165c156ac7312d9eeae
juliolmuller <juliolmuller@outlook.com>
2023-05-16 01:36:16 -0300

    chore: configure ESlint

---
+++ mobile/prettier.config.js
@@ -0,0 +1,5 @@
+const tailWindCssPlugin = require('prettier-plugin-tailwindcss')
+
+module.exports = {
+  plugins: [tailWindCssPlugin],
+}
--- mobile/src/App.tsx
+++ mobile/src/App.tsx
@@ -1,12 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import { Text, View } from 'react-native'
 
 export default function App() {
   return (
     <View className="flex-1 items-center justify-center bg-gray-950">
-      <Text className="text-zinc-50 text-4xl font-bold">👋 Hello, there!</Text>
+      <Text className="text-4xl font-bold text-zinc-50">👋 Hello, there!</Text>
 
       <StatusBar style="light" />
     </View>
-  );
+  )
 }
