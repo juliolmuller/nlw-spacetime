52c38a29d496881e4eec1471e395a42971995f0e
juliolmuller <juliolmuller@outlook.com>
2023-05-15 23:35:48 -0300

    feat: configure Prisma in the project

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,36 @@
+import { PrismaClient } from '@prisma/client'
 import fastify from 'fastify'
 
 const port = Number(process.env.PORT) || 8080
+const prisma = new PrismaClient()
 const app = fastify()
 
 app.get('/', () => {
   return 'Hello, there!'
+})
+
+app.get('/users', async () => {
+  const users = await prisma.user.findMany()
+
+  return users
+})
+
+app.get('/users/:id', async (request) => {
+  const { id } = request.params as any
+  const user = await prisma.user.findFirst({
+    where: { id },
+  })
+
+  return user
+})
+
+app.post('/users', async (request) => {
+  const { name } = request.body as any
+  const newUser = await prisma.user.create({
+    data: { name },
+  })
+
+  return newUser
 })
 
 app.listen({ port }).then(() => {
