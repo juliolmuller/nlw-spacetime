5d5920643570654d45f5f7a5a4d168329d871511
juliolmuller <juliolmuller@outlook.com>
2023-05-18 01:33:19 -0300

    refactor: move database configuration to 'lib' folder

--- server/src/routes/users.routes.ts
+++ server/src/routes/users.routes.ts
@@ -1,5 +1,5 @@
 import { FastifyInstance } from 'fastify'
-import { prisma } from '../database'
+import { prisma } from '../lib/prisma'
 
 export async function makeUsersRoutes(app: FastifyInstance) {
   app.get('/users', async () => {
