acb2a154d8450e2919623ebd06f7fdc10f89933c
juliolmuller <juliolmuller@outlook.com>
2023-05-17 23:13:17 -0300

    style: configure fonts

--- mobile/src/App.tsx
+++ mobile/src/App.tsx
@@ -1,10 +1,22 @@
+import { BaiJamjuree_700Bold as BaiJamjuree700 } from '@expo-google-fonts/bai-jamjuree'
+import {
+  Roboto_400Regular as Roboto400,
+  Roboto_700Bold as Roboto700,
+} from '@expo-google-fonts/roboto'
+import { useFonts } from 'expo-font'
 import { StatusBar } from 'expo-status-bar'
 import { Text, View } from 'react-native'
 
 export default function App() {
+  const [areFontsLoaded] = useFonts({ BaiJamjuree700, Roboto400, Roboto700 })
+
+  if (!areFontsLoaded) {
+    return null
+  }
+
   return (
     <View className="flex-1 items-center justify-center bg-gray-950">
-      <Text className="text-4xl font-bold text-zinc-50">👋 Hello, there!</Text>
+      <Text className="text-4xl font-normal text-zinc-50">🚀RocketSeat</Text>
 
       <StatusBar style="light" />
     </View>
