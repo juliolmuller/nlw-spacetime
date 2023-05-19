f53c4549268676678a42c406eb3244875df5ef3a
juliolmuller <juliolmuller@outlook.com>
2023-05-18 22:49:37 -0300

    feat: add different response status

--- server/src/routes/users.routes.ts
+++ server/src/routes/users.routes.ts
@@ -8,21 +8,29 @@ export async function usersRoutes(app: FastifyInstance) {
     return users
   })
 
-  app.get('/users/:id', async (request) => {
+  app.get('/users/:id', async (request, response) => {
     const { id } = request.params as any
     const user = await prisma.user.findFirst({
       where: { id },
     })
 
+    if (!user) {
+      response.code(404)
+      return {
+        message: 'User does not exist',
+      }
+    }
+
     return user
   })
 
-  app.post('/users', async (request) => {
+  app.post('/users', async (request, response) => {
     const { avatarUrl, githubId, login, name } = request.body as any
     const newUser = await prisma.user.create({
       data: { avatarUrl, githubId, login, name },
     })
 
+    response.code(201)
     return newUser
   })
 }
