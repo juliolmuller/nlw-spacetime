32fc8ce077c4062c735e05b25773e33a1588525f
juliolmuller <juliolmuller@outlook.com>
2023-05-19 23:30:26 -0300

    feat: drop users-related endpoint

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,8 @@
 import cors from '@fastify/cors'
 import jwt from '@fastify/jwt'
 import fastify from 'fastify'
-import { usersRoutes } from './routes/users.routes'
-import { memoriesRoutes } from './routes/memories.routes'
 import { authRoutes } from './routes/auth.routes'
+import { memoriesRoutes } from './routes/memories.routes'
 
 const port = Number(process.env.PORT) || 8080
 const app = fastify()
@@ -15,7 +14,6 @@ app.get('/', () => {
 app.register(cors, { origin: true })
 app.register(jwt, { secret: process.env.JWT_SECRET! })
 app.register(authRoutes)
-app.register(usersRoutes)
 app.register(memoriesRoutes)
 
 app.listen({ port }).then(() => {
