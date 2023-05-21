cbbd1ab7a01addd17c55784333921224efef2616
juliolmuller <juliolmuller@outlook.com>
2023-05-21 01:43:50 -0300

    feat: configure Fastify to be visible by local network

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,12 @@
 import cors from '@fastify/cors'
 import jwt from '@fastify/jwt'
 import fastify from 'fastify'
+import { internalIpV4Sync } from 'internal-ip'
 import { authRoutes } from './routes/auth.routes'
 import { memoriesRoutes } from './routes/memories.routes'
 
 const port = Number(process.env.PORT) || 8080
+const host = '0.0.0.0'
 const app = fastify()
 
 app.get('/', () => {
@@ -16,6 +18,9 @@ app.register(jwt, { secret: process.env.JWT_SECRET! })
 app.register(authRoutes)
 app.register(memoriesRoutes)
 
-app.listen({ port }).then(() => {
-  console.info(`🚀 Server running at http://localhost:${port}`)
+app.listen({ port, host }).then(() => {
+  console.info(`
+    🚀 Server is up!
+       • Local machine URL: http://localhost:${port}
+       • Local network URL: http://${internalIpV4Sync()}:${port}`)
 })
