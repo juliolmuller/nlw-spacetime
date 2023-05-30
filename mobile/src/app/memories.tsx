4902517305c87830c2903f0a259a299c78419e55
juliolmuller <juliolmuller@outlook.com>
2023-05-30 00:45:27 -0300

    feat: create screen to list saved memories

--- mobile/src/app/memories.tsx
+++ mobile/src/app/memories.tsx
@@ -1,18 +1,53 @@
 import Icon from '@expo/vector-icons/Feather'
+import dayjs from 'dayjs'
+import ptBR from 'dayjs/locale/pt-br'
+import { getItemAsync } from 'expo-secure-store'
 import { Link } from 'expo-router'
-import { Text, TouchableOpacity, View } from 'react-native'
+import { Image, ScrollView, Text, TouchableOpacity, View } from 'react-native'
 import { useSafeAreaInsets } from 'react-native-safe-area-context'
 import SpacetimeLogo from '../assets/nlw-spacetime-h-logo.svg'
+import { api } from '../lib/api'
+import { useEffect, useState } from 'react'
+
+type Memory = {
+  id: string
+  coverUrl: string
+  excerpt: string
+  createdAt: string
+}
+
+dayjs.locale(ptBR)
 
 export default function MemoriesScreen() {
   const { bottom, top } = useSafeAreaInsets()
+  const [, setLoading] = useState(false)
+  const [memories, setMemories] = useState<Memory[]>([])
+
+  async function fetchMemories() {
+    try {
+      setLoading(true)
+      const token = await getItemAsync('token')
+      const response = await api.get('/memories', {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+
+      setMemories(response.data)
+    } catch {
+      // do nothing
+    } finally {
+      setLoading(false)
+    }
+  }
+
+  useEffect(() => {
+    fetchMemories()
+  }, [])
 
   return (
-    <View
-      className="flex-1 px-8"
-      style={{ paddingBottom: bottom, paddingTop: top }}
-    >
-      <View className="mt-6 w-full flex-row items-center justify-between">
+    <View className="flex-1" style={{ paddingBottom: bottom, paddingTop: top }}>
+      <View className="mt-6 w-full flex-row items-center justify-between px-8">
         <SpacetimeLogo />
 
         <Link asChild href="/new-memory">
@@ -22,9 +57,44 @@ export default function MemoriesScreen() {
         </Link>
       </View>
 
-      <View className="flex-1 items-center justify-center">
-        <Text>👋 Hello, there!</Text>
-      </View>
+      <ScrollView
+        className="mt-6 flex-1 space-y-10"
+        contentContainerStyle={{ paddingBottom: 60 }}
+      >
+        {memories.map(({ coverUrl, createdAt, excerpt, id }) => (
+          <View key={`memory-${id}`}>
+            <View className="space-y-4">
+              <View className="flex-row items-center gap-2">
+                <View className="h-px w-5 bg-gray-50" />
+                <Text className="text-sm font-normal text-gray-50">
+                  {dayjs(createdAt).format('D[ de ]MMMM[, ]YYYY')}
+                </Text>
+              </View>
+
+              <View className="space-y-4 px-8">
+                <Image
+                  className="aspect-video w-full rounded-lg"
+                  source={{ uri: coverUrl }}
+                  alt=""
+                />
+
+                <Text className="text-base font-normal leading-relaxed text-gray-100">
+                  {excerpt}
+                </Text>
+
+                <Link href={`/memories/${id}`} asChild>
+                  <TouchableOpacity className="flex-row items-center gap-2">
+                    <Text className="text-sm font-normal text-gray-200">
+                      Ler Mais
+                    </Text>
+                    <Icon name="arrow-right" color="#9e9ea0" size={16} />
+                  </TouchableOpacity>
+                </Link>
+              </View>
+            </View>
+          </View>
+        ))}
+      </ScrollView>
     </View>
   )
 }
