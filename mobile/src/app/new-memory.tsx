125e1595e86317c3add3aabd1ec3dec1fb905718
juliolmuller <juliolmuller@outlook.com>
2023-05-29 23:38:28 -0300

    feat: add submission of new memories

--- mobile/src/app/new-memory.tsx
+++ mobile/src/app/new-memory.tsx
@@ -1,7 +1,9 @@
 import Icon from '@expo/vector-icons/Feather'
+import * as ImagePicker from 'expo-image-picker'
 import { useRouter } from 'expo-router'
 import { useState } from 'react'
 import {
+  Image,
   ScrollView,
   Switch,
   Text,
@@ -11,12 +13,73 @@ import {
 } from 'react-native'
 import { useSafeAreaInsets } from 'react-native-safe-area-context'
 import SpacetimeLogo from '../assets/nlw-spacetime-h-logo.svg'
+import { getItemAsync } from 'expo-secure-store'
+import { api } from '../lib/api'
 
 export default function NewMemoryScreen() {
   const router = useRouter()
   const { bottom, top } = useSafeAreaInsets()
+  const [isLoading, setLoading] = useState(false)
   const [isPublic, setPublic] = useState(false)
+  const [coverPreview, setCoverPreview] = useState<string>()
   const [content, setContent] = useState('')
+
+  async function handlePickImage() {
+    const result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    })
+
+    !result.canceled &&
+      result.assets[0] &&
+      setCoverPreview(result.assets[0].uri)
+  }
+
+  async function handleSubmit() {
+    setLoading(true)
+
+    try {
+      const token = await getItemAsync('token')
+      let coverUrl = ''
+
+      if (coverPreview) {
+        const uploadFormData = new FormData()
+        uploadFormData.append('file', {
+          name: 'image.png',
+          type: 'image/png',
+          uri: coverPreview,
+        } as any)
+
+        const response = await api.post('/files', uploadFormData, {
+          headers: {
+            Authorization: `Bearer ${token}`,
+            'Content-Type': 'multipart/form-data',
+          },
+        })
+        coverUrl = response.data.url
+      }
+
+      await api.post(
+        '/memories',
+        {
+          isPublic,
+          content,
+          coverUrl,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        },
+      )
+
+      router.replace('memories')
+    } finally {
+      setLoading(true)
+    }
+  }
 
   return (
     <View
@@ -51,13 +114,24 @@ export default function NewMemoryScreen() {
           </Text>
         </View>
 
-        <TouchableOpacity className="bg-black/2 h-32 justify-center rounded-lg border border-dashed border-gray-500">
-          <View className="flex-row items-center justify-center gap-2">
-            <Icon name="image" color="#fff" />
-            <Text className="inline-flex max-w-[80%] text-center text-sm font-normal text-gray-200">
-              Adicionar foto ou vídeo de capa sdfs dfs df
-            </Text>
-          </View>
+        <TouchableOpacity
+          className="bg-black/2 h-32 justify-center rounded-lg border border-dashed border-gray-500"
+          onPress={handlePickImage}
+        >
+          {coverPreview ? (
+            <Image
+              className="h-full w-full rounded-lg object-cover"
+              source={{ uri: coverPreview }}
+              alt="preview da imagem selecionada"
+            />
+          ) : (
+            <View className="items-center justify-center gap-2">
+              <Icon name="image" color="#fff" size={24} />
+              <Text className="inline-flex max-w-[80%]  text-center text-sm font-normal text-gray-200">
+                Adicionar foto ou vídeo de capa sdfs dfs df
+              </Text>
+            </View>
+          )}
         </TouchableOpacity>
 
         <TextInput
@@ -65,12 +139,16 @@ export default function NewMemoryScreen() {
           placeholderTextColor="#56565a"
           placeholder="Fique livre para adicionar fotos, vídeos e relatos sobre essa experiência que você quer lembrar para sempre."
           multiline
+          textAlignVertical="top"
           value={content}
+          onChangeText={setContent}
         />
 
         <TouchableOpacity
           className="items-center rounded-full bg-green-500 px-5 py-2"
+          disabled={isLoading}
           activeOpacity={0.7}
+          onPress={handleSubmit}
         >
           <Text className="font-alt text-sm uppercase text-black">Salvar</Text>
         </TouchableOpacity>
